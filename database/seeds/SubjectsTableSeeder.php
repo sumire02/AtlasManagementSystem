b02135cbafc0b9794e5fa17482df012f55d01ade
佐藤すみれ <>
2022-12-08 12:19:05 +0900

    Subjects,Users初期データの登録

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -13,5 +13,23 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+
+        DB::table('subjects')->insert([
+            [
+            'id' =>'1',
+            'Subject' =>'国語',
+            'created_at' =>date('Y-m-d H:i:s'),
+            ],
+            [
+            'id' =>'2',
+            'Subject' =>'数学',
+            'created_at' =>date('Y-m-d H:i:s'),
+            ],
+            [
+            'id' =>'3',
+            'Subject' =>'英語',
+            'created_at' =>date('Y-m-d H:i:s'),
+            ],
+        ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->([
+        DB::table('users')->insert([
             [
-
-            ]
-        ])
+                'over_name' =>'先生',
+                'under_name' =>'代表',
+                'over_name_kana' =>'センセイ',
+                'under_name_kana' =>'ダイヒョウ',
+                'mail_address' =>'sennsei@gmail.com',
+                'sex' =>0,
+                'birth_day' =>'1990-01-01',
+                'role' =>1,
+                'password' =>bcrypt('password'),
+                'created_at' =>new DateTime(),
+                'updated_at' =>new DateTime(),
+                'deleted_at' =>new DateTime(),
+            ],
+            [
+                'over_name' =>'生徒',
+                'under_name' =>'一員',
+                'over_name_kana' =>'セイト',
+                'under_name_kana' =>'イチイン',
+                'mail_address' =>'seito@gmail.com',
+                'sex' =>1,
+                'birth_day' =>'2000-12-31',
+                'role' =>2,
+                'password' =>bcrypt('password'),
+                'created_at' =>new DateTime(),
+                'updated_at' =>new DateTime(),
+                'deleted_at' =>new DateTime(),
+            ],
+        ]);
 
     }
 }
