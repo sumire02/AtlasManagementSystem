81480146a0b525b4b1d751731dd00a40cbe14232
佐藤すみれ <>
2022-12-31 23:51:22 +0900

    コメント数の表示途中

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -11,7 +11,8 @@
       <div class="post_bottom_area d-flex">
         <div class="d-flex post_status">
           <div class="mr-5">
-            <i class="fa fa-comment"></i><span class=""></span>
+            <!-- コメント数 -->
+            <i class="fa fa-comment"></i><span class="">{{ $post->id->post_id->count() }}</span>
           </div>
           <div>
             @if(Auth::user()->is_Like($post->id))
